c79e3dd4909e86f80bcbf4e07337caa6070f06b8
Surbhi Sharma <sharma.surbhi13@gmail.com>
2017-10-16 11:56:53 -0400

    added express

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const env = 'development';
 const config = require('./knexfile.js')[env];
 const knex = require('knex')(config);
+
+//require express
+const express = require('express');
+const app = express();
+
+app.listen(4000, function () {
+  console.log('connection to port 4000...');
+});
